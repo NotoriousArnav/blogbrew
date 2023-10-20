171c4550a9c23c0be3668dd13043fa1d8add936f
Eliot Alderson <e.alderson@allsafe.com>
2023-10-21 02:46:47 +0530

    Blog List and Detail View

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", index, name="landing")
+    path("", index, name="landing"),
+    path("blogs/", BlogListView.as_view(), name="Blogs"),
+    path('blogs/<slug:slug>/', BlogDetailView.as_view(), name='blog-detail')
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from user_agents import parse
 import json
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
 @csrf_exempt
@@ -12,3 +14,27 @@ def index(req):
     if ua.is_mobile:
         return render(req, "landing-mobile.html")
     return render(req, "landing.html")
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog_list.html'  # Replace with the actual template name
+    context_object_name = 'blog_posts'
+    ordering = ['-created_at']  # Optional: to order the posts by creation date
+
+    # Optional: Add any additional context data if needed
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add additional context data here
+        return context
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_detail.html'  # Replace with the actual template name
+    context_object_name = 'blog_post'
+    slug_url_kwarg = 'slug'  # Define the slug URL keyword name
+
+    # Optional: Add any additional context data if needed
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add additional context data here
+        return context
