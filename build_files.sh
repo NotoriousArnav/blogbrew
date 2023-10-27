b441dc66049a9ac477b5dc0593ee4ee109c7b995
Eliot Alderson <e.alderson@allsafe.com>
2023-10-27 22:43:18 +0530

    Psycopg2 > Psycopg2-binary

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
  #!/bin/sh
  echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
+ python3.10 -m pip install -r requirements.txt
+ python3.10 manage.py collectstatic --noinput --clear
  echo "BUILD END"
