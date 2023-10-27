0570059e76b320f86d3a3742199beffc64fc4157
Eliot Alderson <e.alderson@allsafe.com>
2023-10-27 23:16:12 +0530

    Fixed Static

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -176,7 +176,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
