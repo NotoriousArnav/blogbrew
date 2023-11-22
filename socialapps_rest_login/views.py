750c8ed2cb8eba44720b1d798b083906acda39a6
Eliot Alderson <e.alderson@allsafe.com>
2023-11-22 23:21:11 +0530

    Private Blogs

--- socialapps_rest_login/views.py
+++ socialapps_rest_login/views.py
@@ -23,7 +23,12 @@ class Profile(DetailView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['user_posts'] = Post.objects.filter(author=context['user_profile'].user)
+        context['user_posts'] = Post.objects.filter(author=context['user_profile'].user, public=True)
+        if self.request.user.is_authenticated:
+            if self.request.user.username == context['user_profile'].user.username:
+                print("Inside Profile View")
+                context['user_posts'] = Post.objects.filter(author=self.request.user)
+                print(context['user_posts'])
         return context
 
 class ProfilePicture(DetailView):
